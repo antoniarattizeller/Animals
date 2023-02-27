c8f06863d2a5e3feafa21509356a152e3008d315
antoniarattizeller <99041903+antoniarattizeller@users.noreply.github.com>
2023-02-27 13:07:55 +0100

    default code for creating a generic animal
    created a Kotlin file named Animals and inside a Animal class declared as superclass with properties and functions every animal has.

--- src/main/kotlin/Animals.kt
+++ src/main/kotlin/Animals.kt
@@ -1,2 +1,28 @@
-class Animals {
+
+//provides a default code for creating a generic animal
+//declaring 'open' tells the compiler that this class can be used as superclass
+open class Animal {
+    //Animal class properties:
+    //declating 'open' to be able to override the image, food, habitat properties
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10 //var because hunger changes
+
+    //declaring 'open' to be able to override the function in subclass
+    open fun makeNoise() {
+        println("The Animal is making a noise")
+    }
+
+    open fun eat() {
+        println("The Animal is eating")
+    }
+
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
 }
